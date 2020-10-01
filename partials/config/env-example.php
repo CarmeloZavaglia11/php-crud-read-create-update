297c467861cf95b56384887be71cbf922f49b04e
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-10-01 13:52:48 +0200

    Complete

--- partials/config/env-example.php
+++ partials/config/env-example.php
@@ -4,5 +4,10 @@ $username = "username";
 $password = "password";
 $dbname = "dbhotel";
 
+$host = 'host';
+$folder = 'folder';
+
+$basepath = $host.$folder;
+
 //---****ALERT****---
 // SOSTITUIRE I CAMPI CON I PROPRI E RINOMINARE IL FILE TOGLIENDO LA DICITURA '-example'
